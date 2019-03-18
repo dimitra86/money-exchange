4aea1469766498d89bc6670dcdbce06de05e67e5
zliden01 <zliden01@protonmail.com>
2019-03-19 00:05:52 +0300

    feat: add function makeExchange

--- src/index.js
+++ src/index.js
@@ -7,28 +7,284 @@ module.exports = function makeExchange(currency) {
     if(currency>10000){
         return {error: "You are rich, my friend! We don't have so much coins for exchange"};
     }
-    else if (currency>1) {
+    else if (currency>0) {
         H2=currency/H;
-        H3=~~H2;
-        currency2=currency-(H3*H);
+        H3=H2^0;
+        if(H3<1){
+                        currency2=currency-(H3*H);
+
+                    Q2=currency2/Q;
+                    Q3=Q2^0;
+                    if (Q3<1){
+                                currency3=currency2-(Q3*Q);
+
+                            D2=currency3/D;
+                            D3=D2^0;
+                            if (D3<1) {
+                                currency4=currency3-(D3*D);
+
+                                N2=currency4/N;
+                                N3=N2^0;
+                                if (N3<1){
+                                    currency5=currency4-(N3*N);
+
+                                    P2=currency5/P;
+                                    P3=P2^0;
+                                    if(P3<1){
+                                        return{};
+                                    }
+                                    else{
+                                    return{"P":P3};
+                                }
+                                }
+                                else {
+                                    currency5=currency4-(N3*N);
+                                    P2=currency5/P;
+                                    P3=P2^0;
+                                    if(P3<1){
+                                    return{"N":N3};
+                                    }
+                                else{
+                                    return{"N":N3,"P":P3};
+                                }
+                            }
+                            }
+                            else{
+                                currency4=currency3-(D3*D);
+                                N2=currency4/N;
+                            N3=N2^0;
+                            if (N3<1){
+                                currency5=currency4-(N3*N);
+
+                                P2=currency5/P;
+                                P3=P2^0;
+                                if(P3<1){
+                                    return{"D":D3};
+                                }
+                                else{
+                                return{"D":D3,"P":P3};
+                                }
+                                }
+                            else {
+                                currency5=currency4-(N3*N);
+                                P2=currency5/P;
+                                P3=P2^0;
+                                if(P3<1){
+                                return{"D":D3,"N":N3};
+                                }
+                                else{
+                                    return{"D":D3,"N":N3,"P":P3};
+                                }
+                            }
+                            }
+                            }
+                    
+                    else{  
+                        currency3=currency2-(Q3*Q);
+
+                    D2=currency3/D;
+                    D3=D2^0;
+                    if (D3<1) {
+                        currency4=currency3-(D3*D);
+
+                        N2=currency4/N;
+                        N3=N2^0;
+                        if (N3<1){
+                            currency5=currency4-(N3*N);
+
+                            P2=currency5/P;
+                            P3=P2^0;
+                            if(P3<1){
+                                return{"Q":Q3};
+                            }
+                            else{
+                            return{"Q":Q3,"P":P3};
+                        }
+                        }
+                        else {
+                            currency5=currency4-(N3*N);
+                            P2=currency5/P;
+                            P3=P2^0;
+                            if(P3<1){
+                            return{"Q":Q3,"N":N3};
+                            }
+                        else{
+                            return{"Q":Q3,"N":N3,"P":P3};
+                        }
+                    }
+                    }
+                    else{
+                        currency4=currency3-(D3*D);
+                        N2=currency4/N;
+                    N3=N2^0;
+                    if (N3<1){
+                        currency5=currency4-(N3*N);
+
+                        P2=currency5/P;
+                        P3=P2^0;
+                        if(P3<1){
+                            return{"Q":Q3,"D":D3};
+                        }
+                        else{
+                        return{"Q":Q3,"D":D3,"P":P3};
+                        }
+                        }
+                    else {
+                        currency5=currency4-(N3*N);
+                        P2=currency5/P;
+                        P3=P2^0;
+                        if(P3<1){
+                        return{"Q":Q3,"D":D3,"N":N3};
+                        }
+                        else{
+                            return{"Q":Q3,"D":D3,"N":N3,"P":P3};
+                        }
+                    }
+                    }
+
+                    }
+                    }
+
+
+        else {
+            currency2=currency-(H3*H);
 
         Q2=currency2/Q;
-        Q3=~~Q2;
-        currency3=currency2-(Q3*Q);
+        Q3=Q2^0;
+        if (Q3<1){
+                    currency3=currency2-(Q3*Q);
+
+                D2=currency3/D;
+                D3=D2^0;
+                if (D3<1) {
+                    currency4=currency3-(D3*D);
+
+                    N2=currency4/N;
+                    N3=N2^0;
+                    if (N3<1){
+                        currency5=currency4-(N3*N);
+
+                        P2=currency5/P;
+                        P3=P2^0;
+                        if(P3<1){
+                            return{"H":H3};
+                        }
+                        else{
+                        return{"H":H3,"P":P3};
+                    }
+                    }
+                    else {
+                        currency5=currency4-(N3*N);
+                        P2=currency5/P;
+                        P3=P2^0;
+                        if(P3<1){
+                        return{"H":H3,"N":N3};
+                        }
+                    else{
+                        return{"H":H3,"N":N3,"P":P3};
+                    }
+                }
+                }
+                else{
+                    currency4=currency3-(D3*D);
+                    N2=currency4/N;
+                N3=N2^0;
+                if (N3<1){
+                    currency5=currency4-(N3*N);
+
+                    P2=currency5/P;
+                    P3=P2^0;
+                    if(P3<1){
+                        return{"H":H3,"D":D3};
+                    }
+                    else{
+                    return{"H":H3,"D":D3,"P":P3};
+                    }
+                    }
+                else {
+                    currency5=currency4-(N3*N);
+                    P2=currency5/P;
+                    P3=P2^0;
+                    if(P3<1){
+                    return{"H":H3,"D":D3,"N":N3};
+                    }
+                    else{
+                        return{"H":H3,"D":D3,"N":N3,"P":P3};
+                    }
+                }
+                }
+                }
+        
+        else{  
+            currency3=currency2-(Q3*Q);
 
         D2=currency3/D;
         D3=D2^0;
-        currency4=currency3-(D3*D);
+        if (D3<1) {
+            currency4=currency3-(D3*D);
 
-        N2=currency4/N;
+            N2=currency4/N;
+            N3=N2^0;
+            if (N3<1){
+                currency5=currency4-(N3*N);
+
+                P2=currency5/P;
+                P3=P2^0;
+                if(P3<1){
+                    return{"H":H3,"Q":Q3};
+                }
+                else{
+                return{"H":H3,"Q":Q3,"P":P3};
+            }
+            }
+            else {
+                currency5=currency4-(N3*N);
+                P2=currency5/P;
+                P3=P2^0;
+                if(P3<1){
+                return{"H":H3,"Q":Q3,"N":N3};
+                }
+            else{
+                return{"H":H3,"Q":Q3,"N":N3,"P":P3};
+            }
+        }
+        }
+        else{
+            currency4=currency3-(D3*D);
+            N2=currency4/N;
         N3=N2^0;
-        currency5=currency4-(N3*N);
+        if (N3<1){
+            currency5=currency4-(N3*N);
 
-        P2=currency5/P;
-        P3=P2^0;
+            P2=currency5/P;
+            P3=P2^0;
+            if(P3<1){
+                return{"H":H3,"Q":Q3,"D":D3};
+            }
+            else{
+             return{"H":H3,"Q":Q3,"D":D3,"P":P3};
+            }
+            }
+        else {
+            currency5=currency4-(N3*N);
+            P2=currency5/P;
+            P3=P2^0;
+            if(P3<1){
+             return{"H":H3,"Q":Q3,"D":D3,"N":N3};
+            }
+            else{
+                return{"H":H3,"Q":Q3,"D":D3,"N":N3,"P":P3};
+            }
+        }
+        }
+
+        }
+        }
+
         
-        return{"H":H3,"Q":Q2,"D":D3,"N":N3,"P":P3};
 
+        
+        
     }
     else{
         return {};
